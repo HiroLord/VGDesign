860f385cf5c578d62f405040ace49f0daddf9b91
justinnle <le.justin94@gmail.com>
2015-10-03 17:34:40 -0400

    ammo pickup
    added prefab for ammo pickup and simple disappear script

---
+++ Map/Assets/Scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 0, 1f);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		//PlayerScript pscript = other.GetComponent<PlayerScript>();
+		Destroy (this.gameObject);
+	}
+}
