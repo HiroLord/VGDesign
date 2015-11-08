32e75f583ba9b0d5262e3fa7685f3961896101e5
SolemanKirby <solemankirby@gmail.com>
2015-11-08 17:50:53 -0500

    Entrance Exit Script
    Script to be attached to exits for use with navigating between levels;
    contains a built-in cooldown timer upon loading into a level and allows
    for setting of a spawn location where the player appears in the level if
    not at the default location.

---
+++ AlphaLevel/Assets/Scripts/EntranceExit.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntranceExit : MonoBehaviour {
+
+    //Test varaible
+    public bool isExit = false;
+
+    //String name of next level
+    public string nextLevel;
+    //Destination coordinates
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+    //Cooldown timer
+    public float countdownMax = 1f;
+    private float countdown = 0f;
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        countdown = countdownMax;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (countdownMax > 0)
+        {
+            countdown -= Time.deltaTime;
+        }
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject == player)
+        {
+            isExit = true;
+            if (countdown <= 0)
+            {
+                //Application.LoadLevel(nextLevel);
+                player.transform.position = new Vector3(x, y, z);
+                countdown = countdownMax;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject == player)
+        {
+            isExit = false;
+        }
+    }
+}
