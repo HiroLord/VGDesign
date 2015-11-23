1f955669b53583246c410231f7c3fb85ffb5b5b9
Sarah Alsmiller <ramvling@gmail.com>
2015-11-23 17:01:59 -0500

    Player/Enemy health slider + prefabs

---
+++ AlphaLevel/Assets/Scripts/UI/HealthSlider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthSlider : MonoBehaviour {
+	Slider slider;
+	Entity entity;
+
+	void Start() {
+		slider = gameObject.GetComponentInChildren<Slider> ();
+		entity = gameObject.GetComponentInParent<Entity> ();
+	}
+		
+	void Update() {
+		slider.value = entity.currentHealth;
+	}
+}
