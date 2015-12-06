f300fe8ac58ae2395afe208e6b557735e70a1575
Sarah Alsmiller <ramvling@gmail.com>
2015-12-06 17:36:39 -0500

    Level and prefab edits

--- AlphaLevel/Assets/Scripts/DragonBoss/Fireball.cs
+++ AlphaLevel/Assets/Scripts/DragonBoss/Fireball.cs
@@ -9,11 +9,13 @@ using System.Collections;
 public class Fireball : MonoBehaviour {
 	Vector3 dest;
 	Vector3 cache;
+	AudioSource src;
 	public static void createFireball(GameObject fireball, Vector3 position, Vector3 destination) {
 		GameObject ball = (GameObject) Instantiate(fireball, position, Quaternion.identity);
 		Fireball ballScript = ball.AddComponent<Fireball> ();
 		Vector3 adjusted = new Vector3 (destination.x, destination.y - 0.75f, destination.z);
 		ballScript.setDestination (adjusted);
+		ballScript.src = ball.GetComponent<AudioSource> ();
 	}
 
 	void Update() {
@@ -34,6 +36,7 @@ public class Fireball : MonoBehaviour {
 
 	//I know this is bad practice to have it in two places. Will fix
 	void explode() {
+		src.Play ();
 		var exp = GetComponent<ParticleSystem>();
 		exp.Play();
 		Destroy(gameObject, exp.duration);
