05a1569cd4eb91e2ae8d5c7ae9e9973b87e41ba7
Preston Turner <prestonturner42@gmail.com>
2015-12-06 22:49:19 -0500

    Wrong bool for join.

--- AlphaLevel/Assets/Scripts/UI/MainMenuManager.cs
+++ AlphaLevel/Assets/Scripts/UI/MainMenuManager.cs
@@ -71,7 +71,7 @@ public class MainMenuManager : MonoBehaviour {
 
 	public void selectConnect() {
 		Debug.Log ("Join selected");
-		Begin (true);
+		Begin (false);
 	}
 
 	public void selectHost() {
